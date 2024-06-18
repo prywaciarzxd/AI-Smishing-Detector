2ac8996a5d49f51e140d26d72f05f1d2be83b90a
jadesader <js@jade.jadetown>
2024-06-19 01:09:25 +0400

    Improved functions

--- download_dataset.py
+++ download_dataset.py
@@ -5,19 +5,21 @@ import shutil
 dataset_name = 'galactus007/sms-smishing-collection-data-set'
 
 # Pobierz zestaw danych i rozpakuj go
+print("Downloading dataset...")
 os.system(f'kaggle datasets download -d {dataset_name} --unzip -p smssmishcollection')
 
 # Ścieżki plików i folderów
-src_file = 'smssmishcollection/SMSSmishCollection.txt'
+src_file = 'smssmishcollection/smssmishcollection/SMSSmishCollection.txt'
 dst_file = './SMSSmishCollection.txt'
 dst_folder = 'smssmishcollection'
 
-# Przenieś plik do katalogu nadrzędnego
+# Sprawdź, czy plik SMSSmishCollection.txt istnieje wewnątrz folderu smssmishcollection
 if os.path.exists(src_file):
+    # Przenieś plik do katalogu nadrzędnego
     shutil.move(src_file, dst_file)
     print(f"Moved {src_file} to {dst_file}")
 
-# Usuń nowo utworzony folder
+# Usuń folder smssmishcollection (zawiera dwa poziomy podkatalogu)
 if os.path.exists(dst_folder):
     shutil.rmtree(dst_folder)
     print(f"Removed folder {dst_folder}")
